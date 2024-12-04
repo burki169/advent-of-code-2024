e4c9c09dd896341b6c3341b5aabb67c1833057b4
burki169 <5102072+burki169@users.noreply.github.com>
2024-12-04 19:27:41 +0100

    Day 1

---
+++ Day1/Day1.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode2024.Day1;
+
+public class Day1
+{
+    private readonly string _pathToInputFile;
+
+    public Day1(string pathToInputFile)
+    {
+        _pathToInputFile = pathToInputFile;
+    }
+    
+    public int Solve()
+    {
+        List<int> firstGroupLocations = new();
+        List<int> secondGroupLocations = new();
+        
+        var lines = File.ReadAllLines(_pathToInputFile);
+        foreach (var line in lines)
+        {
+            var values = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            firstGroupLocations.Add(int.Parse(values[0]));
+            secondGroupLocations.Add(int.Parse(values[1]));
+        }
+        firstGroupLocations.Sort();
+        secondGroupLocations.Sort();
+        int totalDiff = 0;
+        for (var i = 0; i < firstGroupLocations.Count; i++)
+        {
+            var diff = Math.Abs(firstGroupLocations[i] - secondGroupLocations[i]);
+            totalDiff += diff;
+        }
+
+        return totalDiff;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2024.Day1;
+
+Console.WriteLine("Hello, World!");
+var day1 = new Day1("day1\\input.txt");
+Console.WriteLine(day1.Solve());
